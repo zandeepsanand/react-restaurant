b2f94fcf6bbc9bfe15750a528d2f5d8c373f938e
zandeepsanand <sandeepsssanand@gmail.com>
2022-07-21 12:56:08 +0530

    logo changed

--- src/component/header/headerRestaurant.js
+++ src/component/header/headerRestaurant.js
@@ -4,7 +4,7 @@ export default function HeaderRestaurant() {
   return (
     <div>
       <div className='container2 mt-4'>
-      <img className='img-fluid'  src="images/bg3.png" alt="restaurant bg" />
+      <img className='img-fluid'  src="images/bg5.png" alt="restaurant bg5" />
       </div>
     
     </div>
